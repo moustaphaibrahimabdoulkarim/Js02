a2a0856d3d7444cef41178e2342935c60ff9df82
moustaphaibrahimabdoulkarim <124594103+moustaphaibrahimabdoulkarim@users.noreply.github.com>
2023-02-14 17:06:40 +0100

    Add files via upload

---
+++ myName.js
@@ -0,0 +1,3 @@
+let myname = 'Daouda';
+let mystr = 'My name is ' + myname + ' and I am well!';
+console.log(mystr);
---
+++ nameLength.js
@@ -0,0 +1,3 @@
+let lastName = 'Hadiza';
+let nameLength = lastName.length;
+console.log(nameLength);
